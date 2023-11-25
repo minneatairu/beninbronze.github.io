7e73a501d8c65bbb72e3918967cac7df333865a9
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2023-11-25 07:34:47 -0500

    test

--- js/app.js
+++ js/app.js
@@ -1,123 +1,136 @@
-// Define a function to create and initialize the eBay component
-function createEbayComponent() {
-  // State variables
-  let searchQuery = 'Benin Bronzes';
-  let searchResults = [];
-  let isLoading = false;
-  let pageNumber = 1;
-  let entriesPerPage = 100;
-  let totalPages = 0;
-  let hasMore = true;
-  let totalEntries = 0;
-  let showInfoPopup = false;
-  let activeTab = 0;
-  let hoveredImage = null;
-  let hoveredImageIndex = null;
 
-  // Function to show the image popup
-  function showImagePopup(image, index) {
-    hoveredImage = image;
-    hoveredImageIndex = index;
-  }
-
-  // Function to hide the image popup
-  function hideImagePopup() {
-    hoveredImage = null;
-  }
-
-  // Function to close the info popup
-  function closePopup() {
-    showInfoPopup = false;
-  }
-
-  // Function to render tab label
-  function renderTabLabel(label, tabIndex) {
-    return activeTab === tabIndex ? `[${label}]` : label;
-  }
-
-  // Event listener to close popup if clicked outside
-  function handleClickOutside(event) {
-    if (popupRef && !popupRef.contains(event.target)) {
-      closePopup();
-    }
-  }
-
-  // Function to toggle info popup
-  function toggleInfoPopup() {
-    showInfoPopup = !showInfoPopup;
-    console.log(showInfoPopup);
-    if (!showInfoPopup) {
-      closePopup();
-    }
-  }
-
-  // Function to handle tab change
-  function handleTabChange(newValue) {
-    activeTab = newValue;
-  }
-
-  // Function to handle search
-  async function handleSearch(value = 1) {
-    isLoading = true;
-    try {
-      const response = await axios.get(
-        `https://proxy.cors.sh/${XEBAY_API_URL}services/search/FindingService/v1?keywords=${searchQuery}&paginationInput.pageNumber=${value}&paginationInput.entriesPerPage=${entriesPerPage}`,
-        {
-          headers: {
-            'X-EBAY-SOA-SECURITY-APPNAME': XEBAY_SOA_SECURITY_APPNAME,
-            'X-EBAY-SOA-GLOBAL-ID': XEBAY_SOA_GLOBAL_ID,
-            'X-EBAY-SOA-OPERATION-NAME': XEBAY_SOA_OPERATION_NAME,
-            'x-cors-api-key': CORS_API_KEY,
-          },
+    // Accordion Settings 
+    const accSettings = {
+      speed: 500, // Animation speed
+      oneOpen: true, // Close all other accordion items if true
+      offsetAnchor: true, // Activate scroll to top for active item
+      offsetFromTop: 180, // In pixels – Scroll to top at what distance
+      scrollTopDelay: 400, // In Milliseconds – Delay before scroll to top 
+      
+      classes: {
+          accordion: 'js-accordion',
+        header: 'js-accordion-header',
+        item: 'js-accordion-item',
+        body: 'js-accordion-body',
+        icon: 'js-accordion-icon',    active: 'active',
+    
+      }
+    };
+    
+    
+    const prefix = accSettings.classes
+    
+    const accordion = (function(){
+      const accordionElem = $(`.${prefix.accordion}`)
+      const accordionHeader = accordionElem.find(`.${prefix.header}`)
+      const accordionItem = $(`.${prefix.item}`)
+      const accordionBody = $(`.${prefix.body}`)
+      const accordionIcon = $(`.${prefix.icon}`)
+      const activeClass = prefix.active
+        
+      return {
+        // pass configurable object literal
+        init: function(settings) {
+          accordionHeader.on('click', function() {
+            accordion.toggle($(this));
+            if(accSettings.offsetAnchor) {
+                setTimeout(() => { 
+                    $('html').animate({scrollTop: $(this).offset().top-accSettings.offsetFromTop}, accSettings.speed);
+                    }, accSettings.scrollTopDelay);
+            }
+          });
+          
+          $.extend(accSettings, settings); 
+          
+          // ensure only one accordion is active if oneOpen is true
+          if(settings.oneOpen && $(`.${prefix.item}.${activeClass}`).length > 1) {
+            $(`.${prefix.item}.${activeClass}:not(:first)`).removeClass(activeClass).find(`.${prefix.header} > .${prefix.icon}`).removeClass(activeClass);
+          }
+          // reveal the active accordion bodies
+          $(`.${prefix.item}.${activeClass}`).find(`> .${prefix.body}`).show();
+          
+        },
+        
+        toggle: function($this) {
+          if(accSettings.oneOpen && $this[0] != $this.closest(accordionElem).find(`> .${prefix.item}.${activeClass} > .${prefix.header}`)[0]) {
+            $this.closest(accordionElem).find(`> .${prefix.item}`).removeClass(activeClass).find(accordionBody).slideUp(accSettings.speed);
+            $this.closest(accordionElem).find(`> .${prefix.item}`).find(`> .${prefix.header} > .${prefix.icon}`).removeClass(activeClass);
+          }
+          
+          // show/hide the clicked accordion item
+          $this.closest(accordionItem).toggleClass(`${activeClass}`).find(`> .${prefix.header} > .${prefix.icon}`).toggleClass(activeClass);
+          $this.next().stop().slideToggle(accSettings.speed);
         }
-      );
-
-      const result = xml2js(response.data);
-      const items = result.findItemsByKeywordsResponse.searchResult[0].item;
-      const entriesPerPage = result.findItemsByKeywordsResponse.paginationOutput[0].entriesPerPage;
-      const totalPages = result.findItemsByKeywordsResponse.paginationOutput[0].totalPages;
-      const totalEntries = result.findItemsByKeywordsResponse.paginationOutput[0].totalEntries;
-
-      searchResults = items;
-      pageNumber = value;
-      entriesPerPage = entriesPerPage[0];
-      totalPages = totalPages[0];
-      totalEntries = totalEntries[0];
-      isLoading = false;
-    } catch (error) {
-      isLoading = false;
-    }
-  }
-
-  // Function to handle focus
-  function handleFocus() {
-    setIsFocused(true);
-  }
-
-  // Function to handle blur
-  function handleBlur() {
-    setIsFocused(false);
-  }
-
-  // Function to handle change
-  function handleChange(value) {
-    handleSearch(value);
-  }
-
-  // Return an object with public methods and state variables
-  return {
-    toggleInfoPopup,
-    handleTabChange,
-    showImagePopup,
-    hideImagePopup,
-    handleSearch,
-    handleFocus,
-    handleBlur,
-    handleChange,
-    // Include other state variables as needed
-  };
-}
-
-// Initialize the eBay component
-const ebayComponent = createEbayComponent();
-
+      }
+    })();
+    
+    $(document).ready(function(){
+      accordion.init(accSettings);
+    });
+    </script>
+    
+    <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.3.0/model-viewer.min.js"></script>
+    
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      // Function to show popup message
+      function showPopupMessage() {
+        const popup = document.createElement('div');
+        popup.textContent = 'DRAG THE 3D MODEL TO MOVE';
+        popup.style.position = 'fixed';
+        popup.style.top = '20px';
+        popup.style.left = '50%';
+         popup.style.lineHeight = '1';
+          popup.style.fontSize= '2rem';
+            popup.style.color = 'limegreen';
+             popup.style.border = '1px dashed limegreen';
+        popup.style.fontFamily = 'pixelifysans';
+        popup.style.transform = 'translateX(-50%)';
+        popup.style.backgroundColor = '#000000';
+        popup.style.padding = '0px 10px';
+        popup.style.boxShadow = '0 2px 5px rgba(0,0,0,0.2)';
+        popup.style.zIndex = '1000';
+        document.body.appendChild(popup);
+    
+        // Remove the popup after 10 seconds
+        setTimeout(() => {
+          document.body.removeChild(popup);
+        }, 10000);
+      }
+    
+      // Add click event listener to each button
+      const loadButtons = document.querySelectorAll('.load-button');
+      loadButtons.forEach(button => {
+        button.addEventListener('click', function() {
+          const targetSelector = this.getAttribute('data-target');
+          const modelViewer = document.querySelector(targetSelector);
+          if (modelViewer) {
+            // Check if the model has been dismissed
+            if (modelViewer.hasAttribute('dismissed')) {
+              // Reset the poster
+              modelViewer.toggleAttribute('dismissed', false);
+              modelViewer.showPoster();
+              this.textContent = "VIEW 3D SCAN";
+              this.style.backgroundColor = "";
+            } else {
+              // Dismiss the poster and show popup
+              modelViewer.toggleAttribute('dismissed', true);
+              modelViewer.dismissPoster();
+              this.textContent = "CLOSE 3D SCAN";
+              this.style.backgroundColor = "yellow";
+              showPopupMessage(); // Show the popup message
+            }
+          }
+        });
+      });
+    
+      // Set up posters for each model viewer
+      const modelViewers = document.querySelectorAll('.lazy-load');
+      modelViewers.forEach((modelViewer) => {
+        const posterDiv = modelViewer.nextElementSibling;
+        if (posterDiv) {
+          posterDiv.style.backgroundImage = `url(${modelViewer.getAttribute('poster')})`;
+          modelViewer.removeAttribute('poster');
+        }
+      });
+    });
